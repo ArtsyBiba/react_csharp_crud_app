a403a283d461eb6484083ff59bfa30ee2570ff33
Artsy Biba <biba.artsiom@gmail.com>
2021-05-26 11:09:09 -0400

    refactoring the app to be a book database vs a todo database

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,22 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Link { get; set; }
+        public int YearRead { get; set; }
+    }
+}
